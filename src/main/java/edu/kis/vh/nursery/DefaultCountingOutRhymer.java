f04901341b3a55e648a56d559a5281240b611b2a
AdamSzymanskii <117367082+AdamSzymanskii@users.noreply.github.com>
2024-05-26 13:17:59 +0200

    Update DefaultCountingOutRhymer.java

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,12 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int MIN = -1;
-    public static final int AMOUNT_OF_NUMBERS = 12;
-    public static final int NUMBER_COUNTER = 11;
-    private final int[] numbers = new int[AMOUNT_OF_NUMBERS];
+    public static final int EMPTY = -1;
+    public static final int PEEKABOO_MIN = -1;
+    public static final int MAX_SIZE = 12;
+    private final int[] numbers = new int[MAX_SIZE];
 
-    public int total = MIN;
+    public int total = EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
@@ -15,22 +15,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == MIN;
+        return total == EMPTY;
     }
 
     public boolean isFull() {
-        return total == NUMBER_COUNTER;
+        return total == MAX_SIZE - 1;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return MIN;
+            return PEEKABOO_MIN;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return MIN;
+            return EMPTY;
         return numbers[total--];
     }
 
