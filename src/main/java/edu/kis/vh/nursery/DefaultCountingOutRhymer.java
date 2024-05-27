d51782a92f92089ede3843aa634674baed8a7932
HannaSauchuk <248664@edu.p.lodz.pl>
2024-05-27 20:55:51 +0200

    10. Dodanie getter do zmiennej total

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -7,6 +7,10 @@ public class DefaultCountingOutRhymer {
     private static final int MAX_SIZE = 12;
     private final int[] numbers = new int[MAX_SIZE];
 
+    public int getTotal() {
+        return total;
+    }
+
     private int total = EMPTY;
 
     public void countIn(int in) {
